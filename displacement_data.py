739b0569f0c1431d1c3e2f39f3b5c663ff71859c
Miraj Parekh <mirajp272@gmail.com>
2026-06-26 14:48:07 -0400

    added folder for .tsv data from qualisys, and created a file to manipulate tsv data to eventually find displacement for each trial

---
+++ displacement_data.py
@@ -0,0 +1,13 @@
+import csv
+
+# reviewing 2d arrays as representing matrices
+# data = [[1, 1.2, 1.3, 1.4, 1.5], [2, 2.2, 2.3, 2.4, 2.5], [2, 2.2, 2.3, 2.4, 2.5]]
+# for row in data:
+#     for col in row:
+#         print(col)
+
+
+# open csv file and parse only needed data:
+# (x_init, y_init, z_init) & (x_final, y_final, z_final)
+# calculate displacement as d = sqrt((x_final-x_init)^2 + (y_final-y_init)^2 + (z_final-z_init)^2)
+
